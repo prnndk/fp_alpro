079b376d03f62c0ea2a4ef9d2a678cc97af8adba
LaptopMerah” <iqballramadhan18@gmail.com>
2024-04-04 16:13:04 +0700

    feat: add LandingPage

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LandingPageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,12 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-Route::get('/', function () {
-    return view('testing');
-});
+
+// Route::get('/', function () {
+//     return view('testing');
+// });
+Route::get('/', [LandingPageController::class, 'index'])->name('landingpage');
+
+Route::get('/as', [LandingPageController::class, 'index2'])->name('landingpage');
 //login
-Route::get('/login', [\App\Http\Controllers\AuthController::class,'login'])->name('login');
-Route::post('/login', [\App\Http\Controllers\AuthController::class,'authenticate'])->name('postLogin');
-Route::resource('users', \App\Http\Controllers\UserController::class);
+Route::get('/login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'authenticate'])->name('postLogin');
+Route::resource('/users', \App\Http\Controllers\UserController::class);
 Route::resource('/pelanggan', \App\Http\Controllers\PelangganController::class);
-Route::resource('/pemilik',\App\Http\Controllers\PemilikController::class);
+Route::resource('/pemilik', \App\Http\Controllers\PemilikController::class);
