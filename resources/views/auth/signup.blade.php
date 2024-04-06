0b1b1f1bbef885b0597a0370e24657cb7e4c877d
LaptopMerah” <iqballramadhan18@gmail.com>
2024-04-06 15:41:13 +0700

    feat: make sign up page

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,16 +1,33 @@
 @extends('layouts.auth')
-@section('auth', 'Log In')
+@section('auth', 'Sign Up')
+
 @section('content')
     <div class="row h-100">
+        <div class="col-lg-7 d-none d-lg-block">
+            <div id="auth-right">
+            </div>
+        </div>
         <div class="col-lg-5 col-12">
             <div id="auth-left">
+
                 <div class="auth-logo">
                     <a href="/"><img src="{{ asset('assets/static/images/logo/logo.svg') }}" alt="Logo"></a>
                 </div>
-                <h1 class="auth-title">Log in.</h1>
-                <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
-                <form action="{{route('postLogin')}}" method="POST">
+                <h1 class="auth-title">Sign Up.</h1>
+                <p class="auth-subtitle mb-5">Create an account with your information to complete the registration
+                    process.</p>
+                <form action="{{route('postSignUp')}}" method="POST">
                     @csrf
+                    <div class="form-group position-relative has-icon-left mb-4">
+                        <input type="text" class="form-control form-control-xl @error('name') is-invalid @enderror"
+                               placeholder="Input Name Here" name="name" id="name" required>
+                        <div class="form-control-icon">
+                            <i class="bi bi-text-paragraph"></i>
+                        </div>
+                        @error('name')
+                        <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
                     <div class="form-group position-relative has-icon-left mb-4">
                         <input type="text" class="form-control form-control-xl @error('email') is-invalid @enderror"
                                placeholder="Email Address" name="email" id="email" required>
@@ -32,24 +49,11 @@
                         <div class="text-danger">{{ $message }}</div>
                         @enderror
                     </div>
-                    <div class="form-check form-check-lg d-flex align-items-end">
-                        <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault"
-                               name="remember">
-                        <label class="form-check-label text-gray-600" for="flexCheckDefault">
-                            Keep me logged in
-                        </label>
-                    </div>
-                    <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                    <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Sign Up</button>
                 </form>
                 <div class="text-center mt-5 text-lg fs-4">
-                    <p class="text-gray-600">Don't have an account? <a href="/signup" class="font-bold">Sign
-                            up</a>.</p>
-                    <p><a class="font-bold" href="#">Forgot password?</a>.</p>
+                    <p class="text-gray-600">Already have an account? <a href="/login" class="font-bold">Log in</a>.</p>
                 </div>
-            </div>
-        </div>
-        <div class="col-lg-7 d-none d-lg-block">
-            <div id="auth-right">
             </div>
         </div>
     </div>
